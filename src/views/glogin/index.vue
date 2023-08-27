d46321caec438bf34bfc620738ec9fae40309334
jostenlin <jostenlin.gmail.com>
2023-08-28 00:25:10 +0800

    hi

--- src/views/glogin/index.vue
+++ src/views/glogin/index.vue
@@ -10,6 +10,7 @@ import firebase from "firebase/compat/app";
 import * as firebaseui from "firebaseui";
 import "firebaseui/dist/firebaseui.css";
 import { getAuth, signOut } from "firebase/auth";
+import { id } from "element-plus/es/locale";
 
 firebase.initializeApp(firebaseConfig);
 const auth = getAuth();
@@ -17,14 +18,34 @@ const auth = getAuth();
 const user = ref(null);
 const isSignedIn = ref(false);
 
+// 使用 async/await 獲取 ID Token
+const getIdToken = async (user: any) => {
+  try {
+    return await user.getIdToken();
+  } catch (error) {
+    console.error("獲取 ID Token 時發生錯誤：", error);
+    return null;
+  }
+};
+
+// 自訂登入流程
+const customLoginFlow = (idToken: string) => {
+  // ...您的自訂邏輯...
+  console.log("idtoken=" + idToken);
+};
+
 const uiConfig = {
   signInFlow: "redirect",
-  signinSuccessUrl: "http://localhost:8080/",
+  signinSuccessUrl: "http://localhost:8848/",
   signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
   callbacks: {
     signInSuccessWithAuthResult(authResult: any) {
       user.value = authResult.user.displayName;
-      console.log(authResult);
+      getIdToken(authResult.user).then(idToken => {
+        if (idToken) {
+          customLoginFlow(idToken);
+        }
+      });
       isSignedIn.value = true;
       console.log("Signed in by user " + user.value);
       // so it doesn't refresh the page
@@ -48,6 +69,7 @@ onMounted(() => {
   ui.start("#firebaseui-auth-container", uiConfig);
 });
 
+// Sign out
 const handleSignOut = async () => {
   try {
     await signOut(auth);
